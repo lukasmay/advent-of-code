86ee9c6f885a2e2b205f41aacaa41ef48c5a62d4
Wesley Sullivan <lukas7may@gmail.com>
2023-01-13 19:16:09 -0700

    Moved Stuff

--- RandomCode/MyMiniMax/MyMiniMax.py
+++ RandomCode/MyMiniMax/MyMiniMax.py
@@ -4,9 +4,6 @@ import platform
 import time
 from os import system
 
-
-
-
 class GameNode:
     def __init__(self, name, value=1, parent=None):
         self.Name = name      # a char
@@ -59,6 +56,43 @@ def parse_data_as_list(fname):
         print (data_as_string)
         data_list = literal_eval(data_as_string)
     return data_list
+
+
+def minimax(state, depth, player):
+    """
+    AI function that choice the best move
+    :param state: current state of the board
+    :param depth: node index in the tree (0 <= depth <= 9),
+    but never nine in this case (see iaturn() function)
+    :param player: an human or a computer
+    :return: a list with [the best row, best col, best score]
+    """
+    if player == COMP:
+        best = [-1, -1, -infinity]
+    else:
+        best = [-1, -1, +infinity]
+
+    if depth == 0 or game_over(state):
+        score = evaluate(state)
+        return [-1, -1, score]
+
+    for cell in empty_cells(state):
+        x, y = cell[0], cell[1]
+        state[x][y] = player
+        score = minimax(state, depth - 1, -player)
+        state[x][y] = 0
+        score[0], score[1] = x, y
+
+        if player == COMP:
+            if score[2] > best[2]:
+                best = score  # max value
+        else:
+            if score[2] < best[2]:
+                best = score  # min value
+
+    return best
+
+
 
 def num_empty_positions(state):
     """Finds and Returns the amount of unplaced spaces left on the grid"""
