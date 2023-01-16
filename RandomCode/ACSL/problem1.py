0b464fe805f61bf3042c4153f64ba0dc4c72183c
lukasmay <76849604+lukasmay@users.noreply.github.com>
2023-01-16 18:20:37 +0000

    Finished ACSL coding problem

--- RandomCode/ACSL/problem1.py
+++ RandomCode/ACSL/problem1.py
@@ -1,34 +1,36 @@
-def count_up(base, step=1, count=10, start=0):
-    result = []
-    for i in range(count):
-        num = start + i*step
-        digits = []
-        while num:
-            digits.append(int(num % base))
-            num = num // base
-        result.append(''.join(map(str, digits[::-1])))
+def count_up(num, base):
+    digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    num = int(num, base)
+    num += 1
+    result = ''
+    while num > 0:
+        result = digits[num % base] + result
+        num = num // base
     return result
 
 
-
 def findModeCount(num, base, start):
-    if isinstance(start, int):
-        result_num = count_up(base, count=num, start=int(start))
-    else:
-        result_num = count_up(base, count=num, start=int(start, base))
+    start = str(start)
+    result_num = []
+    result_num.append(start)
+    for i in range(num-1):
+        result_num.append(count_up(start, base))
+        start = result_num[i+1]
     digit_count = []
+    count = 1
     for i in result_num:
+        #print(str(count) + ": " + i)
+        #print(i)
+        count += 1
         for j in i:
             digit_count.append(j)
     most_common = max(set(digit_count), key=digit_count.count)
     answer = 0
+    #print(most_common)
     for i in digit_count:
         if i == most_common:
             answer += 1
     return answer
 
 
-
-
-
-print(findModeCount(25,2,1111011))
+print(findModeCount(10,16,'ABCDEF'))
