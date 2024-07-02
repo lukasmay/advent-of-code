f06c56f5b44310744063e6bb54fb15de5bd025d9
Lukas May <jlukasmay@gmail.com>
2024-07-02 14:12:09 -0600

    Part 1 Day 5 Year 2023

--- lib/src/main/java/org/y2023/day5/Day5.java
+++ lib/src/main/java/org/y2023/day5/Day5.java
@@ -1,40 +1,62 @@
 package org.y2023.day5;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
+import java.util.Scanner;
 
 public class Day5 {
     public static void main(String[] args) {
         try {
-            part1("lib/src/main/resources/y2023/day5.txt");
-        } catch (Exception e) {
+            System.out.println(part1("lib/src/main/java/org/y2023/resources/day5.txt"));
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
         
     }
 
-    public static int part1(String input) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(input));
+    // dest start range
+    public static long part1(String input) throws IOException {
+        Scanner scanner = new Scanner(new File(input));
         String line;
-        List<Integer> seeds = new ArrayList<>();
-        Map<Integer, Integer> seedSoil = new HashMap<>();
-
-        seeds.addAll(List.of(reader.readLine().split(" ")).stream().map(Integer::parseInt).toList());
-        reader.readLine();
-        reader.readLine();
+        List<Long> seeds = new ArrayList<>();
+        List<Boolean> visited = new ArrayList<>();
 
 
-        for (int i = 0; i < 7; i++) {
-            while ((line = reader.readLine()) != null) {
-                seedSoil.put(i, Integer.parseInt(line));
+        seeds.addAll(List.of(scanner.nextLine().substring(7).split(" ")).stream().map(Long::parseLong).toList());
+        visited.addAll(Collections.nCopies(seeds.size(), false));
+
+			while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                if (line.equals("")) {
+                    continue;
+                } else if (line.contains("-")) {
+                    Collections.fill(visited, false);
+                    // System.out.println("Line: " + line);
+                    // System.out.println("Seeds: " + seeds.get(2) + "\n");
+                    continue;
+                }
+
+                String[] split = line.split(" ");
+
+                long start = Long.parseLong(split[1]);
+                long conversion = Long.parseLong(split[0]) - start;
+                long max = Long.parseLong(split[2]) + start;
+
+                for (int i = 0; i < seeds.size(); i++) {
+                    Long seed = seeds.get(i);
+                    if (seed >= start && seed <= max && !visited.get(i)) {
+                        visited.set(i, true);
+                        seeds.set(i, conversion + seed);
+                    }
+                }
             }
-            seedSoil.clear();
-        }
-        return 0;
+
+        System.out.println("Location: " + seeds + "\n");
+        scanner.close();
+        seeds.sort((a, b) -> Long.compare(a, b));
+        return seeds.get(0);
     }
 }
